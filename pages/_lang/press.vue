0592f0b13926b2832e367276520272661d59be36
Luis Salas <luisszkl@gmail.com>
2018-04-27 23:15:40 -0400

    New: pluging in assets

--- pages/_lang/press.vue
+++ pages/_lang/press.vue
@@ -6,8 +6,11 @@
 
   <!-- render data from contentful.com -->
   <ul>
-    <li v-for="blogPost in blogPosts" :key="blogPost.fields.title">
-      <h3>{{ blogPost.fields.title }}</h3>
+    <li v-for="p in press" :key="p.fields.title">
+      <h3>{{ p.fields.title }}</h3>
+      <a v-bind:href="p.fields.file.url">Download</a>
+      <v-icon v-if="p.fields.file.contentType != 'application/pdf'">description</v-icon>
+      <v-icon v-else>picture_as_pdf</v-icon>
     </li>
   </ul>
 
@@ -18,7 +21,6 @@
 <script>
 import { createClient } from '~/plugins/contentful.js'
 const client = createClient()
-// var blogPosts = ['1', '2']
 
 export default {
   // html meta data for page
@@ -38,17 +40,22 @@ export default {
   // `env` is available in the context object
   asyncData({ env, store }) {
     return Promise.all([
-      // fetch all blogPosts sorted by creation date
-      client.getEntries({
+      client.getAssets({
         locale: (store.state.locale == 'en')? 'en-US':store.state.locale,
-        content_type: env.CTF_BLOG_POST_TYPE_ID,
         order: '-sys.createdAt',
+        mimetype_group: 'richtext'
       }),
+      client.getAssets({
+        locale: (store.state.locale == 'en')? 'en-US':store.state.locale,
+        order: '-sys.createdAt',
+        mimetype_group: 'pdfdocument'
+      })
     ])
-      .then(([blogPosts]) => {
+      .then(([word, pdf]) => {
         // return data that should be available in the template
+        // console.log('press', word.items[0].fields.file)
         return {
-          blogPosts: blogPosts.items,
+          press: word.items.concat(pdf.items),
         }
       })
       .catch(console.error)
