c86b6db6cc0aebe4bcaa589d775e969eed69bda8
Luis Salas <luisszkl@gmail.com>
2018-06-26 18:35:20 -0400

    Fix: cleaning keys

--- plugins/contentful.js
+++ plugins/contentful.js
@@ -1,12 +1,11 @@
 const contentful = require('contentful')
 const defaultConfig = {
-  CTF_SPACE_ID: process.env.TEST_ID || process.env.CTF_SPACE_ID,
+  CTF_SPACE_ID: process.env.CTF_SPACE_ID,
   CTF_CDA_ACCESS_TOKEN: process.env.CTF_CDA_ACCESS_TOKEN
 }
 
 module.exports = {
   createClient (config = defaultConfig) {
-    console.log('here', process.env.CTF_SPACE_ID)
     return contentful.createClient({
       space: config.CTF_SPACE_ID,
       accessToken: config.CTF_CDA_ACCESS_TOKEN
