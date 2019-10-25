2d74a6ca9246b5a96d069e87adddd2c597db6ecc
Luis Salas <luisszkl@gmail.com>
2019-10-24 22:21:28 -0400

    Refactor: Linting, removes deprecated v-card-media

--- components/article-preview.vue
+++ components/article-preview.vue
@@ -1,21 +1,21 @@
 <template>
   <v-card flat tile>
-    <v-card-media
+    <v-img
       v-if="post.fields.heroImage"
       :src="post.fields.heroImage.fields.file.url + '?fit=scale&w=350&h=196'"
       height="200px"
       weight="200px"
-    ></v-card-media>
-    <v-card-media
+    />
+    <v-img
       v-else
       :src="'https://placeimg.com/200/200/animals'"
       height="200px"
       weight="200px"
-    ></v-card-media>
-    <time class="tiny date">{{ ( new Date(post.fields.publishDate)).toDateString() }}</time>
+    />
+    <time class="tiny date">{{ new Date(post.fields.publishDate).toDateString() }}</time>
     <h4>
       <nuxt-link
-        :to="{ name: 'lang-news-slug', params: { lang: $i18n.locale, slug: post.fields.slug }}"
+        :to="{ name: 'lang-news-slug', params: { lang: $i18n.locale, slug: post.fields.slug } }"
         class="title"
       >{{ post.fields.title }}</nuxt-link>
     </h4>
@@ -24,7 +24,12 @@
 
 <script>
 export default {
-  props: ['post']
+  props: {
+    post: {
+      type: Object,
+      default: null
+    }
+  }
 }
 </script>
 
